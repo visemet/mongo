6a285ffc68b2aa2219bb5ea4687c42277d128faf
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-11-11 16:12:05 -0500

    Add workload for dropDatabase command.

---
+++ jstests/concurrency/workloads/drop_database.js
@@ -0,0 +1,36 @@
+
+/**
+ * drop_database.js
+ *
+ * Repeatedly creates and drops a database.
+ */
+var $config = (function() {
+
+    var states = {
+        init: function init(db, collName) {
+            this.uniqueDBName = 'drop_database' + this.tid;
+        },
+
+        createAndDrop: function createAndDrop(db, collName) {
+            // TODO: should we ever do something different?
+            var myDB = db.getSiblingDB(this.uniqueDBName);
+            assertAlways.commandWorked(myDB.createCollection(collName));
+            assertAlways.commandWorked(myDB.dropDatabase());
+        }
+    };
+
+    var transitions = {
+        init: { createAndDrop: 1 },
+        createAndDrop: { createAndDrop: 1 }
+    };
+
+    return {
+        threadCount: 5,
+        iterations: 100,
+        states: states,
+        transitions: transitions
+    };
+
+})();
+
+
