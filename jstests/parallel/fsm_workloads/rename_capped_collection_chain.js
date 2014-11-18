e2be5491186d551bd0bff86ab97be71aaa243ed1
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-11-18 10:52:19 -0500

    Add workloads for renameCollection command.

---
+++ jstests/parallel/fsm_workloads/rename_capped_collection_chain.js
@@ -0,0 +1,74 @@
+/**
+ * rename_capped_collection_chain.js
+ *
+ * Creates a capped collection and then repeatedly executes the renameCollection
+ * command against it. The previous "to" namespace is used as the next "from"
+ * namespace.
+ */
+var $config = (function() {
+
+    var data = {
+        // Use the workload name as a prefix for the collection name,
+        // since the workload name is assumed to be unique.
+        prefix: 'rename_capped_collection_chain'
+    };
+
+    var states = (function() {
+
+        function uniqueCollectionName(prefix, tid, num) {
+            return prefix + tid + '_' + num;
+        }
+
+        function init(db, collName) {
+            this.fromCollName = uniqueCollectionName(this.prefix, this.tid, 0);
+            this.num = 1;
+
+            var options = {
+                capped: true,
+                size: 4096
+            };
+
+            assertAlways.commandWorked(db.createCollection(this.fromCollName, options));
+            assertWhenOwnDB(db[this.fromCollName].isCapped());
+        }
+
+        function rename(db, collName) {
+            var toCollName = uniqueCollectionName(this.prefix, this.tid, this.num++);
+            var res = db[this.fromCollName].renameCollection(toCollName, false /* dropTarget */);
+            assertWhenOwnDB.commandWorked(res);
+            assertWhenOwnDB(db[toCollName].isCapped());
+            this.fromCollName = toCollName;
+        }
+
+        return {
+            init: init,
+            rename: rename
+        };
+
+    })();
+
+    var transitions = {
+        init: { rename: 1 },
+        rename: { rename: 1 }
+    };
+
+    var teardown = function(db, collName) {
+        var pattern = new RegExp('^' + this.prefix);
+        var res = db.runCommand('listCollections', { filter: { name: pattern } });
+        assertAlways.commandWorked(res);
+
+        res.collections.forEach(function(collInfo) {
+            assertAlways(db[collInfo.name].drop());
+        });
+    };
+
+    return {
+        threadCount: 20,
+        iterations: 100,
+        data: data,
+        states: states,
+        transitions: transitions,
+        teardown: teardown
+    };
+
+})();
