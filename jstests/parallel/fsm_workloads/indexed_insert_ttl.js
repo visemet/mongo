f494171d2b4d32c775bba1d5e8cc7ccdc6d31407
David Percy <david.percy@mongodb.com>
2014-11-19 18:31:03 -0500

    Add remove tests
    also better error message in fsm.js

---
+++ jstests/parallel/fsm_workloads/indexed_insert_ttl.js
@@ -0,0 +1,50 @@
+/**
+ * indexed_insert_ttl.js
+ *
+ * Creates a TTL index with a short expireAfterSeconds value (5 seconds). Each
+ * thread does an insert on each iteration. The first insert done by each
+ * thread is marked with an extra field. At the end, we assert that the first
+ * doc inserted by each thread is no longer in the collection.
+ */
+var $config = (function() {
+
+    var states = {
+        init: function init(db, collName) {
+            var res = db[collName].insert({ t: ISODate(), first: true });
+            assertWhenOwnColl.eq(1, res.nInserted, tojson(res));
+        },
+
+        insert: function insert(db, collName) {
+            var res = db[collName].insert({ t: ISODate() });
+            assertWhenOwnColl.eq(1, res.nInserted, tojson(res));
+        }
+    };
+
+    var transitions = {
+        init: { insert: 1 },
+        insert: { insert: 1 }
+    };
+
+    function setup(db, collName) {
+        db[collName].ensureIndex({ t: 1 }, { expireAfterSeconds: this.ttlSeconds });
+    }
+
+    function teardown(db, collName) {
+        // The TTL thread runs every 60 seconds, so for reliability, wait more than ttlSeconds
+        // plus a minute.
+        sleep((2 * this.ttlSeconds + 70) * 1000);
+        assertWhenOwnColl.eq(0, db[collName].find({ first: true }).itcount());
+    }
+
+    return {
+        threadCount: 30,
+        iterations: 1000,
+        states: states,
+        transitions: transitions,
+        setup: setup,
+        data: {
+            ttlSeconds: 5
+        },
+        teardown: teardown
+    };
+})();
