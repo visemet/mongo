c3285a71cd0b2233aaaf4b11213d130736660a2c
David Percy <david.percy@mongodb.com>
2014-11-19 17:03:39 -0500

    2d and 2dsphere

---
+++ jstests/parallel/fsm_workloads/indexed_insert_2d.js
@@ -0,0 +1,29 @@
+/**
+ * indexed_insert_2d.js
+ *
+ * Inserts documents into an indexed collection and asserts that the documents appear in both a
+ * collection scan and an index scan. The indexed value is a 2-element array of numbers, indexed
+ * with a 2d index.
+ */
+load('jstests/parallel/fsm_libs/runner.js'); // for parseConfig
+load('jstests/parallel/fsm_workloads/indexed_insert_base.js'); // for $config
+
+var $config = extendWorkload($config, function($config, $super) {
+
+    $config.states.init = function(db, collName) {
+        $super.states.init.apply(this, arguments);
+
+        // assume fewer than 180*180 = 32400 threads, so we can assign each thread to a
+        // point
+        assertAlways.lt(this.tid, 180*180);
+        this.indexedValue = [Math.floor(this.tid / 180), this.tid % 180];
+    };
+
+    $config.data.getIndexSpec = function() {
+        var ix = {};
+        ix[this.indexedField] = '2d';
+        return ix;
+    };
+
+    return $config;
+});
