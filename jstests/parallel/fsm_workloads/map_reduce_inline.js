eeba83fc9acf21b7a90d9d1def9390b72701c129
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-11-20 15:53:39 -0500

    Restructure map_reduce_inline.js workload.
    Now is compatible with incremental map-reduce operations using the
    "merge" output action.

--- jstests/parallel/fsm_workloads/map_reduce_inline.js
+++ jstests/parallel/fsm_workloads/map_reduce_inline.js
@@ -1,52 +1,57 @@
 /**
  * map_reduce_inline.js
  *
- * TODO: write description
+ * Generates some random data and inserts it into a collection. Runs a
+ * map-reduce command over the collection that computes the frequency
+ * counts of the 'value' field in memory.
+ *
+ * Used as the base workload for the other map-reduce workloads.
  */
 var $config = (function() {
 
-    var data = {
-        numDocs: 2000
-    };
-
-    var states = (function() {
-
-        // TODO: modify mapper to produce { this.value : 1 } so that 'out' can be
-        //       changed to replace, merge, or reduce
-        function mapper() {
-            if (this.hasOwnProperty('key') && this.hasOwnProperty('value')) {
-                emit(this.key, this.value);
-            }
+    function mapper() {
+        if (this.hasOwnProperty('key') && this.hasOwnProperty('value')) {
+            var obj = {};
+            obj[this.value] = 1;
+            emit(this.key, obj);
         }
+    }
 
-        function reducer(key, values) {
-            var res = {};
+    function reducer(key, values) {
+        var res = {};
 
-            values.forEach(function(value) {
-                if (typeof value !== 'number') {
-                    return;
-                }
-
+        values.forEach(function(obj) {
+            Object.keys(obj).forEach(function(value) {
                 if (!res.hasOwnProperty(value)) {
                     res[value] = 0;
                 }
-                res[value]++;
+                res[value] += obj[value];
             });
+        });
 
-            return res;
-        }
+        return res;
+    }
 
-        function finalizer(key, reducedValue) {
-            return reducedValue;
-        }
+    function finalizer(key, reducedValue) {
+        return reducedValue;
+    }
+
+    var data = {
+        numDocs: 2000,
+        mapper: mapper,
+        reducer: reducer,
+        finalizer: finalizer
+    };
+
+    var states = (function() {
 
         function init(db, collName) {
             // no-op
         }
 
         function mapReduce(db, collName) {
-            var options = { finalize: finalizer, out: { inline: 1 } };
-            var res = db[collName].mapReduce(mapper, reducer, options);
+            var options = { finalize: this.finalizer, out: { inline: 1 } };
+            var res = db[collName].mapReduce(this.mapper, this.reducer, options);
             assertAlways.commandWorked(res);
         }
 
@@ -74,7 +79,7 @@ var $config = (function() {
         // TODO: do we want to use the bulk API for doing the inserts?
         for (var i = 0; i < this.numDocs; ++i) {
             // TODO: this actually does assume that there are no unique indexes
-            var res = db[collName].insert(makeDoc(this.numDocs / 100, this.numDocs));
+            var res = db[collName].insert(makeDoc(this.numDocs / 100, this.numDocs / 10));
             assertAlways.writeOK(res);
             assertAlways.eq(1, res.nInserted);
         }
@@ -82,7 +87,7 @@ var $config = (function() {
 
     return {
         threadCount: 20,
-        iterations: 100,
+        iterations: 50,
         data: data,
         states: states,
         transitions: transitions,
