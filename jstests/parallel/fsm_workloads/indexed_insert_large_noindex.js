50e1d22d4e248449b4f12f069cde207f0d130bb8
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-11-26 10:19:00 -0500

    Rename fsm_workload_helpers/ to fsm_workload_modifiers.

--- jstests/parallel/fsm_workloads/indexed_insert_large_noindex.js
+++ jstests/parallel/fsm_workloads/indexed_insert_large_noindex.js
@@ -5,6 +5,6 @@
  */
 load('jstests/parallel/fsm_libs/runner.js'); // for extendWorkload
 load('jstests/parallel/fsm_workloads/indexed_insert_large.js'); // for $config
-load('jstests/parallel/fsm_workload_helpers/indexed_noindex.js'); // for indexedNoindex
+load('jstests/parallel/fsm_workload_modifiers/indexed_noindex.js'); // for indexedNoindex
 
 var $config = extendWorkload($config, indexedNoindex);
