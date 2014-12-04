9d8bb1fffbaf1e63ab97f39e8959da3e85ff6f77
David Percy <david.percy@mongodb.com>
2014-12-04 14:31:24 -0500

    _id for sharding

--- jstests/parallel/fsm_workloads/indexed_insert_upsert.js
+++ jstests/parallel/fsm_workloads/indexed_insert_upsert.js
@@ -26,6 +26,7 @@ var $config = extendWorkload($config, function($config, $super) {
     $config.states.insert = function insert(db, collName) {
         var doc = this.getDoc();
         doc.counter = this.counter++; // ensure doc is unique to guarantee an upsert occurs
+        doc._id = ObjectId(); // for sharding
 
         var res = db[collName].update(doc, { $inc: { unused: 0 } }, { upsert: true });
         assertAlways.eq(0, res.nMatched, tojson(res));
