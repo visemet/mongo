788be8ab935514aa61bde9af1c54897509def078
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-04 16:00:41 -0500

    Cannot reproduce failure either.

--- jstests/parallel/fsm_all_composed.js
+++ jstests/parallel/fsm_all_composed.js
@@ -15,7 +15,7 @@ var blacklist = [
 // }));
 
 var whitelist = [
-//    'jstests/parallel/fsm_workloads/update_inc.js',
+    'jstests/parallel/fsm_workloads/update_inc.js',
 //    'jstests/parallel/fsm_workloads/rename_capped_collection_dbname_droptarget.js',
 //    'jstests/parallel/fsm_workloads/drop_collection.js',
 //    'jstests/parallel/fsm_workloads/update_ordered_bulk_inc.js',
@@ -24,7 +24,7 @@ var whitelist = [
 //    'jstests/parallel/fsm_workloads/map_reduce_inline.js',
 //    'jstests/parallel/fsm_workloads/indexed_insert_text_multikey.js',
 //    'jstests/parallel/fsm_workloads/indexed_insert_long_fieldname_noindex.js',
-    'jstests/parallel/fsm_workloads/findAndModify_inc.js',
+//    'jstests/parallel/fsm_workloads/findAndModify_inc.js',
 ];
 
 runCompositionOfWorkloads(whitelist, {}, { numSubsets: 1 });
