6ba72c1369103037d862b7be99f3075d55325b1f
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-06 16:31:34 -0500

    Blacklist workloads that explicitly call db.collection.drop().

--- jstests/parallel/fsm_all.js
+++ jstests/parallel/fsm_all.js
@@ -11,7 +11,12 @@ var blacklist = [
     'drop_collection.js',
     'drop_database.js', // SERVER-16285
     'map_reduce_merge_nonatomic.js', // SERVER-16262
-    'map_reduce_reduce_nonatomic.js' // SERVER-16262
+    'map_reduce_reduce_nonatomic.js', // SERVER-16262
+    'map_reduce_replace_nonexistent.js',
+    'rename_capped_collection_dbname_droptarget.js',
+    'rename_capped_collection_droptarget.js',
+    'rename_collection_dbname_droptarget.js',
+    'rename_collection_droptarget.js',
 ].map(function(file) { return dir + '/' + file; });
 
 runWorkloadsSerially(ls(dir).filter(function(file) {
