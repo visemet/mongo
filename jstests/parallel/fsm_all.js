1216ebdacb937736b419bc476025daf167391aef
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-08 16:52:50 -0500

    Blacklist workloads that fail on RC1.

--- jstests/parallel/fsm_all.js
+++ jstests/parallel/fsm_all.js
@@ -10,6 +10,8 @@ var blacklist = [
     'create_collection.js',
     'drop_collection.js',
     'drop_database.js', // SERVER-16285
+    'indexed_insert_multikey.js',
+    'indexed_insert_text.js',
     'map_reduce_merge.js',
     'map_reduce_merge_nonatomic.js', // SERVER-16262
     'map_reduce_reduce_nonatomic.js', // SERVER-16262
@@ -20,6 +22,7 @@ var blacklist = [
     'rename_collection_dbname_chain.js',
     'rename_collection_dbname_droptarget.js',
     'rename_collection_droptarget.js',
+    'update_multifield_multiupdate.js',
 ].map(function(file) { return dir + '/' + file; });
 
 runWorkloadsSerially(ls(dir).filter(function(file) {
