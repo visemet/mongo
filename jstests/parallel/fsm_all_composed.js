02dd347d9ffc3a2963420d03172cb3566faa53b1
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-09 09:22:57 -0500

    Copy set of blacklisted workloads in fsm_all.js to fsm_all_composed.js.

--- jstests/parallel/fsm_all_composed.js
+++ jstests/parallel/fsm_all_composed.js
@@ -5,9 +5,24 @@ load('jstests/parallel/fsm_libs/runner.js');
 var dir = 'jstests/parallel/fsm_workloads';
 
 var blacklist = [
+    'create_capped_collection.js',
+    'create_capped_collection_maxdocs.js',
+    'create_collection.js',
+    'drop_collection.js',
     'drop_database.js', // SERVER-16285
+    'indexed_insert_multikey.js',
+    'indexed_insert_text.js',
+    'map_reduce_merge.js',
     'map_reduce_merge_nonatomic.js', // SERVER-16262
-    'map_reduce_reduce_nonatomic.js' // SERVER-16262
+    'map_reduce_reduce_nonatomic.js', // SERVER-16262
+    'map_reduce_replace_nonexistent.js',
+    'rename_capped_collection_dbname_chain.js',
+    'rename_capped_collection_dbname_droptarget.js',
+    'rename_capped_collection_droptarget.js',
+    'rename_collection_dbname_chain.js',
+    'rename_collection_dbname_droptarget.js',
+    'rename_collection_droptarget.js',
+    'update_multifield_multiupdate.js',
 ].map(function(file) { return dir + '/' + file; });
 
 runCompositionOfWorkloads(ls(dir).filter(function(file) {
