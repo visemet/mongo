0372e0741dbc1059e880561e38cec96bde429f98
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-10 08:13:01 -0500

    Disable all map-reduce and renameCollection workloads.

--- jstests/parallel/fsm_all_sharded_replication.js
+++ jstests/parallel/fsm_all_sharded_replication.js
@@ -5,6 +5,25 @@ load('jstests/parallel/fsm_libs/runner.js');
 var dir = 'jstests/parallel/fsm_workloads';
 
 var blacklist = [
+    // Disable all map-reduce workloads for now
+    'map_reduce_inline.js',
+    'map_reduce_merge.js',
+    'map_reduce_merge_nonatomic.js',
+    'map_reduce_reduce.js',
+    'map_reduce_reduce_nonatomic.js',
+    'map_reduce_replace.js',
+    'map_reduce_replace_nonexistent.js',
+
+    // Disable all rename collection workloads for now
+    'rename_capped_collection_chain.js',
+    'rename_capped_collection_dbname_chain.js',
+    'rename_capped_collection_dbname_droptarget.js',
+    'rename_capped_collection_droptarget.js',
+    'rename_collection_chain.js',
+    'rename_collection_dbname_chain.js',
+    'rename_collection_dbname_droptarget.js',
+    'rename_collection_droptarget.js',
+
     'drop_database.js', // SERVER-16285
     'indexed_insert_eval.js', // eval doesn't work with sharded collections
     'indexed_insert_eval_nolock.js', // eval doesn't work with sharded collections
