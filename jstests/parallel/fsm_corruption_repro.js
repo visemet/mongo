dff162edda64ced5f82a972b23e7ce85721e011e
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-11 12:44:17 -0500

    Try to disable map-reduce workload using reduce action.

--- jstests/parallel/fsm_corruption_repro.js
+++ jstests/parallel/fsm_corruption_repro.js
@@ -5,7 +5,7 @@ load('jstests/parallel/fsm_libs/runner.js');
 var dir = 'jstests/parallel/fsm_workloads';
 
 var whitelist = [
-    'map_reduce_reduce.js',
+//    'map_reduce_reduce.js',
     'map_reduce_replace.js',
     'update_multifield.js', // triggered it
 ].map(function(file) { return dir + '/' + file; });
