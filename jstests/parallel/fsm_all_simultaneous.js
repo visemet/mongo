e06b95c3e655d328791a715dde5f446c019a7e24
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-11 08:54:45 -0500

    Use same blacklist from composed in simultaneous.

--- jstests/parallel/fsm_all_simultaneous.js
+++ jstests/parallel/fsm_all_simultaneous.js
@@ -5,9 +5,32 @@ load('jstests/parallel/fsm_libs/runner.js');
 var dir = 'jstests/parallel/fsm_workloads';
 
 var blacklist = [
+    'create_capped_collection.js',
+    'create_capped_collection_maxdocs.js',
+    'create_collection.js',
+    'drop_collection.js',
     'drop_database.js', // SERVER-16285
+    'indexed_insert_multikey.js',
+    'indexed_insert_text.js',
+    'indexed_insert_text_multikey.js',
+    'map_reduce_merge.js',
     'map_reduce_merge_nonatomic.js', // SERVER-16262
-    'map_reduce_reduce_nonatomic.js' // SERVER-16262
+    'map_reduce_reduce_nonatomic.js', // SERVER-16262
+    'map_reduce_replace_nonexistent.js',
+    'rename_capped_collection_chain.js',
+    'rename_capped_collection_dbname_chain.js',
+    'rename_capped_collection_dbname_droptarget.js',
+    'rename_capped_collection_droptarget.js',
+    'rename_collection_chain.js',
+    'rename_collection_dbname_chain.js',
+    'rename_collection_dbname_droptarget.js',
+    'rename_collection_droptarget.js',
+    'update_multifield_isolated_multiupdate.js',
+    'update_multifield_isolated_multiupdate_noindex.js',
+    'update_multifield_multiupdate.js',
+    'update_multifield_multiupdate_noindex.js',
+    'update_upsert_multi.js',
+    'update_upsert_multi_noindex.js',
 ].map(function(file) { return dir + '/' + file; });
 
 runWorkloadsInParallel(ls(dir).filter(function(file) {
