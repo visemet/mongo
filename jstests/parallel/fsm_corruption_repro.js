22e335191b9f1af2a95eda2a6dd59e1517642ae7
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-11 12:32:30 -0500

    Try without remove workloads and in-memory MR.

--- jstests/parallel/fsm_corruption_repro.js
+++ jstests/parallel/fsm_corruption_repro.js
@@ -7,8 +7,8 @@ var dir = 'jstests/parallel/fsm_workloads';
 var whitelist = [
     'map_reduce_reduce.js',
     'map_reduce_replace.js',
-    'map_reduce_inline.js',
-    'remove_multiple_documents.js',
+//    'map_reduce_inline.js',
+//    'remove_multiple_documents.js',
     'update_multifield.js', // triggered it
 ].map(function(file) { return dir + '/' + file; });
 
