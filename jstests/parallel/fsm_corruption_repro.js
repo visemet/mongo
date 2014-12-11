c26cfae1948b8ae4fbdbffb46e7f576892720dd2
Max Hirschhorn <max.hirschhorn@mongodb.com>
2014-12-11 13:06:05 -0500

    Confirmed.

--- jstests/parallel/fsm_corruption_repro.js
+++ jstests/parallel/fsm_corruption_repro.js
@@ -5,7 +5,6 @@ load('jstests/parallel/fsm_libs/runner.js');
 var dir = 'jstests/parallel/fsm_workloads';
 
 var whitelist = [
-//    'map_reduce_replace.js',
     'update_multifield.js', // triggered it
 ].map(function(file) { return dir + '/' + file; });
 
