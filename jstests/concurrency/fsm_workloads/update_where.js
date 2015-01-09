64c870b917340dce8c8fcd3781fe7c0860618129
Jonathan Balsano <jonathan.balsano@10gen.com>
2015-01-09 18:19:53 -0500

    Fixed a bad assertAlways() in update_where workload

--- jstests/concurrency/fsm_workloads/update_where.js
+++ jstests/concurrency/fsm_workloads/update_where.js
@@ -24,7 +24,7 @@ var $config = extendWorkload($config, function($config, $super) {
             { $set: { x: Random.randInt(this.randomBound) } },
             { multi: true }
         );
-        assertAlways().writeOK(res);
+        assertAlways.writeOK(res);
 
         if (db.getMongo().writeMode() === 'commands') {
             assertAlways.gte(res.nModified, 0);
