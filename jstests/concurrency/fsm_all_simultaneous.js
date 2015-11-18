a65c4f780c9a3487a1189319482cfe598659f635
Max Hirschhorn <max.hirschhorn@mongodb.com>
2015-11-18 15:15:50 -0500

    Disable indexed_insert_base_capped.js.

--- jstests/concurrency/fsm_all_simultaneous.js
+++ jstests/concurrency/fsm_all_simultaneous.js
@@ -11,6 +11,11 @@ var blacklist = [
     // guaranteed to be true when they are run in parallel with other workloads.
     'list_indexes.js',
     'update_inc_capped.js',
+
+    // indexed_insert_base_capped.js sometimes fails due to "CappedPositionLost CollectionScan died
+    // due to position in capped collection being deleted" when run with worklaods that cause the
+    // server to trigger more frequently yielding.
+    'indexed_insert_base_capped',
 ].map(function(file) { return dir + '/' + file; });
 
 // SERVER-16196 re-enable executing workloads
