3b279f196b1d2b1925ca44dfa5202bc0f8eddd30
Max Hirschhorn <max.hirschhorn@mongodb.com>
2015-11-18 15:15:03 -0500

    Add comment about workloads that cannot be run due to tid range assumptions.

--- jstests/concurrency/fsm_all_simultaneous.js
+++ jstests/concurrency/fsm_all_simultaneous.js
@@ -5,6 +5,10 @@ load('jstests/concurrency/fsm_libs/runner.js');
 var dir = 'jstests/concurrency/fsm_workloads';
 
 var blacklist = [
+    // Disabled due to MongoDB restrictions and/or workload restrictions
+
+    // These workloads implcitly assume that their tid range is [0, $config.threadCount). This isn't
+    // guaranteed to be true when they are run in parallel with other workloads.
     'list_indexes.js',
     'update_inc_capped.js',
 ].map(function(file) { return dir + '/' + file; });
