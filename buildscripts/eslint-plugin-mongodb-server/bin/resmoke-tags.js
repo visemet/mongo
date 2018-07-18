b98b5c6828a48ea01b1f9a4029ce44eeeeb3813e
Max Hirschhorn <max.hirschhorn@mongodb.com>
2018-07-18 09:14:07 -0400

    Add remove-tag and rename-tag commands.

--- buildscripts/eslint-plugin-mongodb-server/bin/resmoke-tags.js
+++ buildscripts/eslint-plugin-mongodb-server/bin/resmoke-tags.js
@@ -1,36 +1,45 @@
 #!/usr/bin/env node
 
 const program = require("commander");
-
 const CLIEngine = require('eslint').CLIEngine;
-const plugins = require("eslint/lib/config/plugins");
+
+function lint(ruleOptions) {
+    const cli = new CLIEngine({
+        plugins: ["mongodb-server"],
+        rules: {"mongodb-server/resmoke-tags": ruleOptions},
+        fix: true,
+    });
+
+    const report = cli.executeOnFiles(files);
+    if (report.errorCount > 0) {
+        console.error(report.results[0].messages);
+        process.exit(2);
+    }
+    CLIEngine.outputFixes(report);
+}
 
 program.command("add-tag <tag> [files...]")
     .description("Adds the resmoke.py tag to the list of files")
     .option("-m|--message [message]", "Optional message to include as a comment for the tag")
     .action((tag, files, cmd) => {
-        console.log("add-tag", tag, files, cmd.message);
-
-        // XXX: Omitting `plugins.load("mongodb-server")` causes the schema of the rules' options to
-        // not get validated for some reason.
-        plugins.load("mongodb-server");
-
-        let options = {tag};
+        const options = {tag};
         if (cmd.message !== undefined) {
             options.comment = cmd.message;
         }
 
-        var cli = new CLIEngine({
-            plugins: ["mongodb-server"],
-            rules: {"mongodb-server/resmoke-tags": ["error", {$_internalAddTag: options}]},
-            fix: true,
-        });
+        lint(["error", {$_internalAddTag: options}]);
+    });
 
-        const report = cli.executeOnFiles(files);
-        if (report.errorCount > 0) {
-            console.error(report.results[0].messages);
-        }
-        CLIEngine.outputFixes(report);
+program.command("remove-tag <tag> [files...]")
+    .description("Removes the resmoke.py tag from the list of files")
+    .action((tag, files) => {
+        lint(["error", {$_internalRemoveTag: {tag}}]);
+    });
+
+program.command("rename-tag <from-tag> <to-tag> [files...]")
+    .description("Renames the resmoke.py tag in the list of files")
+    .action((tag, files) => {
+        lint(["error", {$_internalRenameTag: {from: fromTag, toTag: toTag}}]);
     });
 
 program.parse(process.argv);
