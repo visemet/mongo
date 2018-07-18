da151d45b8001f125559a0410cf7766e1ced2258
Max Hirschhorn <max.hirschhorn@mongodb.com>
2018-07-18 13:22:15 -0400

    Sort output of list-tags and find-tag.

--- buildscripts/eslint-plugin-mongodb-server/bin/resmoke-tags.js
+++ buildscripts/eslint-plugin-mongodb-server/bin/resmoke-tags.js
@@ -6,6 +6,10 @@ const util = require("util");
 const program = require("commander");
 const CLIEngine = require('eslint').CLIEngine;
 
+function localeCompare(str1, str2) {
+    return str1.localeCompare(str2);
+}
+
 function lint(files, ruleOptions) {
     const cli = new CLIEngine({
         // We don't use the top-level .eslintrc.yml file but we still enable ES6 features and
@@ -84,7 +88,7 @@ program.command("list-tags [files...]")
             }
         }
 
-        for (let tag of allTags) {
+        for (let tag of Array.from(allTags).sort(localeCompare)) {
             console.log(tag);
         }
     });
@@ -93,6 +97,7 @@ program.command("find-tag <tag> [files...]")
     .description("Lists the files which use the resmoke.py tag")
     .action((tag, files) => {
         const cwd = process.cwd() + path.sep;
+        const allFiles = new Set();
         const report = lint(files, ["warn", {$_internalListTags: null}]);
 
         for (let result of report.results) {
@@ -105,11 +110,15 @@ program.command("find-tag <tag> [files...]")
                 // ESLint returns 'result.filePath' as an absolute path so we strip off the prefix
                 // to make it a path relative to the current working directory.
                 if (result.filePath.startsWith(cwd)) {
-                    console.log(result.filePath.substring(cwd.length));
+                    allFiles.add(result.filePath.substring(cwd.length));
                 } else {
-                    console.log(result.filePath);
+                    allFiles.add(result.filePath);
                 }
             }
+        }
+
+        for (let filename of Array.from(allFiles).sort(localeCompare)) {
+            console.log(filename);
         }
     });
 
