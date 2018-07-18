0dd348f9310b0cabac863905b0df261c5e63fd51
Max Hirschhorn <max.hirschhorn@mongodb.com>
2018-07-18 10:11:55 -0400

    Fix rename-tag command by naming parameters correctly.

--- buildscripts/eslint-plugin-mongodb-server/bin/resmoke-tags.js
+++ buildscripts/eslint-plugin-mongodb-server/bin/resmoke-tags.js
@@ -45,7 +45,7 @@ program.command("remove-tag <tag> [files...]")
 
 program.command("rename-tag <from-tag> <to-tag> [files...]")
     .description("Renames the resmoke.py tag in the list of files")
-    .action((tag, files) => {
+    .action((fromTag, toTag, files) => {
         lint(files, ["error", {$_internalRenameTag: {from: fromTag, to: toTag}}]);
     });
 
